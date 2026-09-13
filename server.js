8170a32f6c5a00c33bfbdffbb54fd60905d97aa9
htj6hgnngd-prog <reshaet@icloud.com>
2026-09-13 15:49:41 +0300

    Align server port with Railway public domain

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 const root = __dirname;
 
 const types = {
